8f67b30843a4ec6b01e792898bf56e703d14eb84
namsoo5 <are34@naver.com>
2019-12-03 16:35:19 +0900

    뷰전환 애니메이션 연습

---
+++ prac_Animation/prac_Animation/ThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdViewController.swift
+//  prac_Animation
+//
+//  Created by 남수김 on 2019/12/03.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
